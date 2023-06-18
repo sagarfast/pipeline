3fa035e1056b76719e75a1e7dd4672dfa3942e30
sagar <sagar@gmail.com>
2023-06-18 18:42:33 +0530

    commit17

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,7 +6,7 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/sagarfast/student-ui.git'
             }
         }
-        stage('test') { 
+        stage('build') { 
             steps {
                 sh 'mvn clean package'
             }
